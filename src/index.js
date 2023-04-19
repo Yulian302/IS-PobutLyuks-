b207f87e20cb51e10058b014e8874f1b096df881
Yulian302 <bohomolyulian3022003@gmail.com>
2023-04-19 18:36:12 +0300

    added some front end

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './scss/main.scss'
+import './scss/header_content.scss'
+import './scss/footer_content.scss'
 function get_creator_name(name) {
     const element = document.createElement('p');
     let str='This template is created by '+name;
     element.textContent = str;
     return element;
 }
-document.body.prepend(get_creator_name('Yulian Bohomol'));
+// document.body.prepend(get_creator_name('Yulian Bohomol'));
